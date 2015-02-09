cdcf6c5ab59cae42234b0dbbc8a4b6b0ac9ca345
hklauken <hklauken@icloud.com>
2015-02-09 08:01:24 +0800

    R code submission

---
+++ plot3.R
@@ -0,0 +1,10 @@
+library(data.table)
+allData <- read.table("household_power_consumption.txt",header=TRUE,sep=";",na.strings="?")
+twoDayData <- as.data.table(allData[allData$Date=="1/2/2007"|allData$Date=="2/2/2007",])
+twoDayData[,datetime:= as.POSIXct(strptime(paste(Date,Time),"%d/%m/%Y %H:%M:%S"),tz="UTC")]  # must use tz and specify valid time zone to make it work
+png("plot3.png",width=480,height=480)
+with(twoDayData,plot(datetime,Sub_metering_1,type="l",xlab="",ylab="Energy sub metering"))
+with(twoDayData,lines(datetime,Sub_metering_2,col="red"))
+with(twoDayData,lines(datetime,Sub_metering_3,col="blue"))
+legend("topright",lty,lwd=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.off()
